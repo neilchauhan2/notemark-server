024fe7ded47cd0ce9f5e7fde57e28dae7d7f6f58
Neil Chauhan <neilchauhan2@gmail.com>
2020-04-23 00:46:55 +0530

    bookmark routes done

--- routes/Bookmark/index.js
+++ routes/Bookmark/index.js
@@ -1,12 +1,41 @@
 const route = require("express").Router();
-const {} = require("../../controllers/Bookmark");
+const {
+  createBookmark,
+  getAllBookmarks,
+  deleteBookmark
+} = require("../../controllers/Bookmark");
 
 //create bookmark
 route.post("/add", async (req, res) => {
   try {
+    const { title, description, url } = req.body;
+    const bookmark = await createBookmark({ title, description, url });
+    res.send(bookmark);
+  } catch (error) {
+    throw error;
+  }
+});
+
+// get all bookmarks
+route.get("/all/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const bookmarks = await getAllBookmarks(id);
+    res.send(bookmarks);
   } catch (error) {
     throw error;
   }
 });
 
 // delete bookmark
+route.get("/remove/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await deleteBookmark(id);
+    res.send(result);
+  } catch (error) {
+    throw error;
+  }
+});
+
+module.exports = route;
